bcc34a107872e0b0339e10707b0453c3e23e1671
Azat ESER <developer.azat@gmail.com>
2022-04-08 12:52:38 +0300

    stats preview

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     content: ['./src/**/*.{html,js}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'lexend': ['Lexend Deca', 'sans-serif'],
+                'inter': ['Inter', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 };

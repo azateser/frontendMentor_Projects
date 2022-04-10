c765a725634ef5a6c7f458f18353f7a9f3f25875
Azat ESER <developer.azat@gmail.com>
2022-04-11 02:42:45 +0300

    Profile card component
    added Profile card component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                'lexend': ['Lexend DecaLexend Deca', 'sans-serif'],
-                'inter': ['Inter', 'sans-serif'],
-                'shoulders': ['Big Shoulders Display', 'cursive'],
+                lexend: ['Lexend DecaLexend Deca', 'sans-serif'],
+                inter: ['Inter', 'sans-serif'],
+                shoulders: ['Big Shoulders Display', 'cursive'],
+                kumbh: ['Kumbh Sans', 'sans-serif'],
+            },
+            backgroundImage: {
+                'profile-card-bg-top': "url('/frontendMentor_Projects/src/Profile card component/images/bg-pattern-top.svg')",
+                'profile-card-bg-bot': "url('/frontendMentor_Projects/src/Profile card component/images/bg-pattern-bottom.svg')",
+                'profile-card': "url('/frontendMentor_Projects/src/Profile card component/images/bg-pattern-card.svg')",
             },
         },
     },

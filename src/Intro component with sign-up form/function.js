780b13d7daf316a409efed45cef522af8979ba33
Azat ESER <developer.azat@gmail.com>
2022-04-30 16:40:25 +0300

    edit

--- src/Intro component with sign-up form/function.js
+++ src/Intro component with sign-up form/function.js
@@ -1,7 +1,7 @@
-let firstname = document.getElementById('firstname').value;
-let lastname = document.getElementById('lastname').value;
-let email = document.getElementById('email').value;
-let password = document.getElementById('password').value;
+let firstname = document.getElementById('firstname');
+let lastname = document.getElementById('lastname');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
 
 let error_firstname = document.getElementById('error_firstname');
 let error_lastname = document.getElementById('error_lastname');
@@ -9,43 +9,60 @@ let error_email = document.getElementById('error_email');
 let error_password = document.getElementById('error_password');
 
 function send() {
-    if (firstname == '') {
+    if (firstname.value == '') {
         if (error_firstname.style.display == 'none') {
             error_firstname.style.display = '';
-            console.log(error_firstname.style.display);
         } else {
             error_firstname.style.display = '';
-            console.log(error_firstname.style.display);
+        }
+    } else {
+        if (error_firstname.style.display == '') {
+            error_firstname.style.display = 'none';
+        } else {
+            error_firstname.style.display = 'none';
         }
     }
 
-    if (lastname == '') {
+    if (lastname.value == '') {
         if (error_lastname.style.display == 'none') {
             error_lastname.style.display = '';
-            console.log(error_lastname.style.display);
         } else {
             error_lastname.style.display = '';
-            console.log(error_lastname.style.display);
+        }
+    } else {
+        if (error_lastname.style.display == '') {
+            error_lastname.style.display = 'none';
+        } else {
+            error_lastname.style.display = 'none';
         }
     }
 
-    if (email == '') {
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.value.match(mailformat) && email.value != '') {
+        if (error_email.style.display == '') {
+            error_email.style.display = 'none';
+        } else {
+            error_email.style.display = 'none';
+        }
+    } else {
         if (error_email.style.display == 'none') {
             error_email.style.display = '';
-            console.log(error_email.style.display);
         } else {
             error_email.style.display = '';
-            console.log(error_email.style.display);
         }
     }
 
-    if (password == '') {
+    if (password.value == '') {
         if (error_password.style.display == 'none') {
             error_password.style.display = '';
-            console.log(error_password.style.display);
         } else {
             error_password.style.display = '';
-            console.log(error_password.style.display);
+        }
+    } else {
+        if (error_password.style.display == '') {
+            error_password.style.display = 'none';
+        } else {
+            error_password.style.display = 'none';
         }
     }
 }

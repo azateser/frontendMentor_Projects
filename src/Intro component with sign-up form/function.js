254b4d07d0bf1033acedebd6cbc8c0ccf0ab7b52
Azat ESER <developer.azat@gmail.com>
2022-04-30 16:47:16 +0300

    function error fix

--- src/Intro component with sign-up form/function.js
+++ src/Intro component with sign-up form/function.js
@@ -12,12 +12,14 @@ function send() {
     if (firstname.value == '') {
         if (error_firstname.style.display == 'none') {
             error_firstname.style.display = '';
+            firstname.style.border = '2px solid #FF7A7A';
         } else {
             error_firstname.style.display = '';
         }
     } else {
         if (error_firstname.style.display == '') {
             error_firstname.style.display = 'none';
+            firstname.style.border = '2px solid #e5e7eb';
         } else {
             error_firstname.style.display = 'none';
         }
@@ -26,12 +28,14 @@ function send() {
     if (lastname.value == '') {
         if (error_lastname.style.display == 'none') {
             error_lastname.style.display = '';
+            lastname.style.border = '2px solid #FF7A7A';
         } else {
             error_lastname.style.display = '';
         }
     } else {
         if (error_lastname.style.display == '') {
             error_lastname.style.display = 'none';
+            lastname.style.border = '2px solid #e5e7eb';
         } else {
             error_lastname.style.display = 'none';
         }
@@ -41,12 +45,14 @@ function send() {
     if (email.value.match(mailformat) && email.value != '') {
         if (error_email.style.display == '') {
             error_email.style.display = 'none';
+            email.style.border = '2px solid #e5e7eb';
         } else {
             error_email.style.display = 'none';
         }
     } else {
         if (error_email.style.display == 'none') {
             error_email.style.display = '';
+            email.style.border = '2px solid #FF7A7A';
         } else {
             error_email.style.display = '';
         }
@@ -55,12 +61,14 @@ function send() {
     if (password.value == '') {
         if (error_password.style.display == 'none') {
             error_password.style.display = '';
+            password.style.border = '2px solid #FF7A7A';
         } else {
             error_password.style.display = '';
         }
     } else {
         if (error_password.style.display == '') {
             error_password.style.display = 'none';
+            password.style.border = '2px solid #e5e7eb';
         } else {
             error_password.style.display = 'none';
         }

6ccb9723efbbb0272c4b2844a761926e8e6a3389
Azat ESER <developer.azat@gmail.com>
2022-08-19 21:58:38 +0300

    edited

--- src/Interactive card details form/React/src/App.js
+++ src/Interactive card details form/React/src/App.js
@@ -1,7 +1,11 @@
 import Home from './components/Home';
 
 function App() {
-  return <Home />;
+  return 
+  <>
+  <Home />;
+  <div class="attribution">Challenge by <a class="text-gray-500 font-medium" href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. Coded by <a class="text-indigo-500 font-bold" href="https://github.com/azateser">Azat ESER</a>.</div>
+  </>
 }
 
 export default App;

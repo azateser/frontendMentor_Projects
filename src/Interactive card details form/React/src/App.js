bbb044b32fe26ffb31058e934990abbf40141245
Azat ESER <developer.azat@gmail.com>
2022-08-19 22:04:09 +0300

    white screen fixed

--- src/Interactive card details form/React/src/App.js
+++ src/Interactive card details form/React/src/App.js
@@ -1,11 +1,12 @@
 import Home from './components/Home';
 
 function App() {
-  return 
+  return (
   <>
   <Home />;
   <div class="attribution">Challenge by <a class="text-gray-500 font-medium" href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. Coded by <a class="text-indigo-500 font-bold" href="https://github.com/azateser">Azat ESER</a>.</div>
   </>
+  )
 }
 
 export default App;
